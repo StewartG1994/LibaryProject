24da39b010904d0f2b063e79ef49c8232290b3a5
Stewart Gliniecki <stewgliniecki@gmail.com>
2021-09-18 19:26:36 +0100

    Working prototype toggle button

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const submit = document.querySelector('.submit');
 const cardContainer = document.querySelector('.cardContainer')
 let read = document.querySelector('.read');
 
-
 let bookArray = [];
 let dataAttribute = 0;
 
@@ -13,12 +12,10 @@ function book (title, author,read,pages){
     this.pages = pages
 }
 
-
-
-book.prototype.toggleStatus = function (){
-   console.log(this.read)
+book.prototype.toggle = function (){
+    if (this.read === true) {this.read = false}
+    else if (this.read === false) {this.read = true}
 }
-
 
 
 submit.addEventListener('click', addBookToLibary)
@@ -30,20 +27,20 @@ function addBookToLibary(){
     const pages = document.querySelector('.pages').value
     let read = document.querySelector('.read');
 
-
     if (document.querySelector('.read').checked){
-        read = 'Read' 
+        read = true
     }
-    else {read = 'Not Read' }
+    else {read = false }
 
     let newBook = new book (title, author, read, pages)
-    newBook.toggleStatus()
+
     bookArray.push(newBook);
    
     document.querySelector('.title').value = ''
     document.querySelector('.author').value = ''
     document.querySelector('.pages').value = ''
     document.querySelector('.read').checked = ''
+
     renderBook()
 }
 
@@ -58,7 +55,7 @@ function renderBook(){
     let read = document.createElement('p')
     let deleteBtn = document.createElement('button');
     let readBtn = document.createElement('readBtn');
-
+    read.classList.add('.readP')
 
     readBtn.classList.add('readBtn');
     readBtn.textContent = 'Change Read Status';
@@ -66,8 +63,15 @@ function renderBook(){
     deleteBtn.textContent = 'Delete Book';
 
     bookArray.forEach(function (item, index) {
-        
+    
+        readBtn.addEventListener('click', () =>{
+            item.toggle();
+            if (item.read === false) {read.textContent = 'Not Read'}
+            else { read.textContent = 'Book Read'}
+            })
+      
     card.setAttribute('data-number', index)
+    readBtn.setAttribute('data-button', index)
     let  titleVal = item.title;
     let authorVal = item.author;
     let  pagesVal = item.pages;
@@ -92,3 +96,4 @@ deleteBtn.addEventListener('click', event =>{
     bookArray.pop(parseInt(data))
     cardContainer.removeChild(cardDelete);})
 }
+
